0e237214c0232e823ddedeaba261430852a11fe6
Marius Parvu <iamlookingforthebathroom@yahoo.com>
2025-02-01 21:32:45 +0200

    Reworked sprites

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main( int argc, char* argv[] )  {
     Canvas canvas( "Mines");
-    std::vector<std::string> themes = { "Digital clock", "Rolling clock", "Steampunk clock" };
+    std::vector<std::string> themes = { "Steampunk clock", "Digital clock" ,"Rolling clock" };
     std::make_shared<Game>( themes, canvas )->Run();
  }
 
