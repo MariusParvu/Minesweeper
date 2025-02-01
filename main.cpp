48a7d86b24cb6d75ed50d2f21ad6b6816f56f98f
Marius Parvu <iamlookingforthebathroom@yahoo.com>
2025-02-01 21:37:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main( int argc, char* argv[] )  {
     Canvas canvas( "Mines");
+
     std::vector<std::string> themes = { "Steampunk clock", "Digital clock" ,"Rolling clock" };
     std::make_shared<Game>( themes, canvas )->Run();
  }
